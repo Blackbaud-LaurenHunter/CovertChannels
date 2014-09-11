2c8399adcb434b1741fede797b7f8023cdef3969
kyle.martinez <kmartinez@convio.com>
2014-09-11 14:51:31 -0500

    read instructions from command line

--- src/com/company/SecureSystem.java
+++ src/com/company/SecureSystem.java
@@ -1,10 +1,28 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class SecureSystem {
 
     public static void main(String[] args) {
-	// write your code here
+        BufferedReader reader = null;
 
+        try {
 
+            String currentInstruction;
+            reader = new BufferedReader(new FileReader(args[0]));
+            while ((currentInstruction = reader.readLine()) != null) {
+                System.out.println(currentInstruction);
+
+                //Project code goes here
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
     }
 }
